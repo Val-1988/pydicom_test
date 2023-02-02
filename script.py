b38438619e9bedb959bbd6c01eb9e97b0a41cb11
Valentina <ivankovavm@gmail.com>
2023-02-02 16:21:39 +0300

    Added new dirs, new names for files .cmd

---
+++ script.py
@@ -0,0 +1,24 @@
+import glob
+import os
+from pydicom.filereader import dcmread
+
+path_my = os.getcwd()
+directory = 'src'
+
+for path in glob.iglob(f'{directory}/*.dcm'):
+
+    dataset = dcmread(path)
+    first_lay = str(dataset.StudyInstanceUID)
+    second_lay = str(dataset.SeriesInstanceUID)
+    abspath = path_my + "/" + first_lay + "/" + second_lay
+    name = str(dataset.SOPInstanceUID)
+
+    if not os.path.exists(abspath):
+        os.makedirs(abspath)
+
+    if hasattr(dataset, "PatientName"):
+        dataset.PatientName = "anonymous"
+    else:
+        print("This file", path, "has no Attribute PatientName")
+
+    dataset.save_as(abspath + "/" + name + ".dcm")
